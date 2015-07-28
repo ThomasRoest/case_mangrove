9b194758c561d4ac04a7b9e792767898d19032d2
thomas roest <thomasroest@outlook.com>
2015-07-28 11:01:35 +0200

    set ENV vars

--- lib/Rijksmuseum.rb
+++ lib/Rijksmuseum.rb
@@ -7,11 +7,11 @@ module Rijksmuseum
 
   
   def self.search(query)
-    get("/collection?q=#{query}&key=05vWA47V&format=json").parsed_response['artObjects']
+    get("/collection?q=#{query}&key=#{ENV['rijks_api_key']}&format=json").parsed_response['artObjects']
     #key in ENV!
   end
 
   def self.top_pieces
-    get("/collection?key=05vWA47V&toppieces=true&ps=40&format=json").parsed_response['artObjects']
+    get("/collection?key=#{ENV['rijks_api_key']}&toppieces=true&ps=40&format=json").parsed_response['artObjects']
   end
 end
