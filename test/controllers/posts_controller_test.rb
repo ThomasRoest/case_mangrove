be894449e1e6a755398845505474071c88359e28
thomas roest <thomasroest@outlook.com>
2015-07-29 14:30:45 +0200

    change h1

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -5,7 +5,7 @@ class PostsControllerTest < ActionController::TestCase
   test "should get index" do 
     get :index
     assert_response :success
-    assert_select "h1", "Alle berichten"
+    assert_select "h1", "Artikelen"
   end
   
 end
